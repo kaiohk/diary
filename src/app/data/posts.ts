75b424f6eea022d927700a046d692bcc687f9831
kaio <kaitozhiroki@gmail.com>
2026-06-15 23:36:28 -0300

    feat: implementing posts by arrays

---
+++ src/app/data/posts.ts
@@ -0,0 +1,18 @@
+export const posts = [
+  {
+    slug: "your-lightness",
+    title: "Your Lightness",
+    description: "Why being with you make things easier?",
+    date: "12/06/2026",
+    image: "/firstImage.jpg",
+    imageAlt: "Amsterdam by night time",
+  },
+  {
+    slug: "gift",
+    title: "Gift",
+    description: "The day you have me a gift",
+    date: "15/06/2026",
+    image: "/firstImage.jpg",
+    imageAlt: "Amsterdam by night time",
+  },
+];
