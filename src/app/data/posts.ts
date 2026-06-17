01a8cdd74b811d693a2350dc51d32ed71c2f805c
kaio <kaitozhiroki@gmail.com>
2026-06-17 17:46:30 -0300

    feat: creating posts / adding images to each post

--- src/app/data/posts.ts
+++ src/app/data/posts.ts
@@ -1,10 +1,32 @@
+export const header = [
+  {
+    image: "/images/profile/me.jpg",
+  },
+];
+
 export const posts = [
   {
     slug: "your-lightness",
     title: "Your Lightness",
     description: "Why being with you make things easier?",
     date: "12/06/2026",
-    image: "/firstImage.jpg",
+    image: "/images/posts/firstImage.jpg",
+    imageAlt: "Amsterdam by night time",
+  },
+  {
+    slug: "gift",
+    title: "Gift",
+    description: "The way you care about me makes me feel good.",
+    date: "16/06/2026",
+    image: "/images/posts/secondImage.jpg",
+    imageAlt: "Cozy Weekend - Beatrice Euphemie",
+  },
+  {
+    slug: "you",
+    title: "You",
+    description: "Small details that I notice about You",
+    date: "17/06/2026",
+    image: "/images/posts/thirdImage.jpg",
     imageAlt: "Amsterdam by night time",
   },
 ];
